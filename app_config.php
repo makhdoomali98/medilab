edc17b9966590b177eb97db786bb47f33544fe67
makhdoomali98 <makhdoomali98@gmail.com>
2021-01-04 21:02:02 +0500

    change app_config merge resolved

--- app_config.php
+++ app_config.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD:app_config.php
- class app_config{
-=======
+
  Class Project_Config{
->>>>>>> f9f1c79669d35e9dc25a0d0e51e20c99118a7b2f:config.php
     public static $host = "localhost";
     public static $dbusername = "root";
     public static $dbpassword = "";
